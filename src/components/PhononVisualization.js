e95579dd4354ba57a88389eb5d11b27056d591b8
superstar54 <xingwang1991@gmail.com>
2024-06-30 09:50:39 +0200

    Hide default GUI of weas

--- src/components/PhononVisualization.js
+++ src/components/PhononVisualization.js
@@ -1,8 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react';
 import * as weas from 'weas';
 
+const defaultGuiConfig = {
+  enabled: false,
+
+  };
+
 function PhononVisualization() {
-  const [fileContent, setFileContent] = useState('');
+
+  const defaultCIFContent = `
+data_image0
+_chemical_formula_structural       Fe2
+_chemical_formula_sum              "Fe2"
+_cell_length_a       2.8
+_cell_length_b       2.8
+_cell_length_c       2.8
+_cell_angle_alpha    90
+_cell_angle_beta     90
+_cell_angle_gamma    90
+
+_space_group_name_H-M_alt    "P 1"
+_space_group_IT_number       1
+
+loop_
+  _space_group_symop_operation_xyz
+  'x, y, z'
+
+loop_
+  _atom_site_type_symbol
+  _atom_site_label
+  _atom_site_symmetry_multiplicity
+  _atom_site_fract_x
+  _atom_site_fract_y
+  _atom_site_fract_z
+  _atom_site_occupancy
+  Fe  Fe1       1.0  0.00000  0.00000  0.00000  1.0000
+  Fe  Fe2       1.0  0.50000  0.50000  0.50000  1.0000
+`;
+
+  const [fileContent, setFileContent] = useState(defaultCIFContent);
   const [eigenvector, setEigenvector] = useState([[1, 0, 0], [0, 0, 1]]);
   const [nframes, setNframes] = useState(20);
   const [amplitude, setAmplitude] = useState(1);
@@ -59,7 +95,7 @@ function PhononVisualization() {
       const phononTrajectory = generatePhononTrajectory(atoms, eigenvector, amplitude, nframes);
 
       if (!weasViewerRef.current) {
-        const editor = new weas.WEAS({ domElement });
+        const editor = new weas.WEAS({ domElement, guiConfig: defaultGuiConfig, viewerConfig: { backgroundColor: '#0000FF' }});
         weasViewerRef.current = editor;
       }
 
